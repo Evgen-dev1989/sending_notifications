e96830482face048086ce0c4b3a914f603763db2
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-01-15 15:50:16 +0200

    send_email

---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config import Config
+
+def send_email(recipient: str, message: str):
+    try:
+        mime_part = MIMEMultipart()
+        mime_part["From"] = Config.user_email
+        mime_part["To"] = recipient
+        mime_part["Subject"] = "Notification"
+        
+        mime_part.attach(MIMEText(message, "plain"))
+        
+        with smtplib.SMTP(Config.host, Config.port) as server:
+            server.starttls()
+            server.login(Config.user_email, Config.password_email)
+            server.sendmail(Config.user_email, recipient, mime_part.as_string())
+        return {"status": "success", "message": "Email sent successfully"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
