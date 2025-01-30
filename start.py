f909b85f6e43543d2c520a529d163b9db0f8aa2a
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-01-30 21:29:57 +0200

    +

--- start.py
+++ start.py
@@ -6,15 +6,22 @@ import asyncio
 async def connect_db():
  
     conn = None
+
     try:
+
         conn = await asyncpg.connect(user=user, password=password, database=database, host=local_host, port=local_port)
-        get_emails = await conn.fetch('SELECT email FROM api')
-        get_emails = await conn.fetch('SELECT email FROM api')
-        return get_emails
+
+        records = await conn.fetch('SELECT email, message FROM api')
+
+        return records
+
     except Exception as e:
+
         print(f"Error: {e}")
         raise
+
     finally:
+        
         if conn:
             await conn.close()
 
@@ -23,21 +30,24 @@ async def main():
     url = "http://127.0.0.1:8000/notify/send_all"
 
     base = await connect_db()
+    for record in base:
+        email = record["email"]
+        message = record["message"]
+        print(f"Email: {email}, Message: {message}")
 
-    emails = [record['email'] for record in base]
 
-    for i in emails:
-        payload = {
-        "recipient": i,
-        "message": "Hi! We'll be glad to see you at our party."
-        }
+    # for i in emails:
+    #     payload = {
+    #     "recipient": i,
+    #     "message": "Hi! We'll be glad to see you at our party."
+    #     }
         
-        headers = {
-            "Content-Type": "application/json"
-        }
-        response = requests.post(url, json=payload, headers=headers)
-        print(response.status_code)
-        print(response.json())
+    #     headers = {
+    #         "Content-Type": "application/json"
+    #     }
+    #     response = requests.post(url, json=payload, headers=headers)
+    #     print(response.status_code)
+    #     print(response.json())
 
 if __name__ == '__main__':
     asyncio.run(main())
